1bf2c431a3cde5efd12e1800d63912f830fc43b7
Kenneth Yang <kjy5@uw.edu>
2024-06-27 15:38:54 -0700

    2 more odd tests...

---
+++ assets/5/file_right.java
@@ -0,0 +1,5 @@
+class Test {
+    private int one = 1;
+    private int somethingElse = 2;
+    private int three = 3;
+}
---
+++ assets/6/file_merged.java
@@ -0,0 +1,6 @@
+class Test {
+
+    private int one = 1;
+
+    private int two = 2;
+}
