f7781c2ff4900dbd5bb91d49610a6136d78873ad
Kenneth Yang <kjy5@uw.edu>
2024-07-11 09:11:12 -0700

    More examples

---
+++ assets/8/file_left.java
@@ -0,0 +1,19 @@
+class Test {
+  public void one() {
+    System.out.println("one");
+    int one = 1;
+    System.out.println("one done");
+  }
+
+  public void three() {
+    int three = 2;
+    if (1 == 1) {
+      System.out.println("three");
+    } else {
+      System.out.println("three");
+    }
+    if (three == 3) {
+      System.out.println("three done");
+    }
+  }
+}
---
+++ assets/8/file_merged.java
@@ -0,0 +1,18 @@
+class Test {
+
+    public void one() {
+        System.out.println("one");
+        int one = 1;
+        System.out.println("one done");
+    }
+
+    public void two() {
+        if (1 == 1) {
+            System.out.println("two");
+        }
+        int two = 2;
+        if (two == 3) {
+            System.out.println("two done");
+        }
+    }
+}
