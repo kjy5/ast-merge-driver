26584b570a646ad1d160c385a95a0dbb4e3bffa6
Kenneth Yang <kjy5@uw.edu>
2024-07-11 10:45:41 -0700

    Creating matchings and example 9

---
+++ assets/9/file_right.java
@@ -0,0 +1,6 @@
+class Test {
+    private int one = 1;
+    private float oneAndAQuarter = 1.25f;
+    private int three = 3;
+    private int two = 2;
+}
--- src/main/java/org/kjy5/Main.java
+++ src/main/java/org/kjy5/Main.java
@@ -1,79 +1,70 @@
 package org.kjy5;
 
-import com.github.javaparser.StaticJavaParser;
-import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.printer.lexicalpreservation.LexicalPreservingPrinter;
+import com.github.gumtreediff.client.Run;
+import com.github.gumtreediff.gen.javaparser.JavaParserGenerator;
+import com.github.gumtreediff.matchers.Matcher;
+import com.github.gumtreediff.matchers.Matchers;
+import com.github.gumtreediff.tree.Tree;
+import com.github.gumtreediff.tree.TreeContext;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class Main {
+  // Constants for the file paths.
+  private static final String ASSETS_FOLDER_PATH = "assets/";
+  private static final String BASE_FILE_PATH = "/file_base";
+  private static final String LEFT_FILE_PATH = "/file_left";
+  private static final String RIGHT_FILE_PATH = "/file_right";
+  private static final String MERGED_FILE_PATH = "/file_merged";
+  private static final String JAVA_FILE_EXTENSION = ".java";
+  private static final String XML_FILE_EXTENSION = ".xml";
+
+  /**
+   * Entry point of the program.
+   *
+   * <p>Runs the full pipeline
+   *
+   * @param args Command line arguments (folder name)
+   */
   public static void main(String[] args) {
     // region File path specifications
-    var folder = args.length > 0 ? args[0] : "0";
-
-    // Constants for the file paths.
-    var assetsPath = "assets/";
-    var fileBaseName = "/file_base";
-    var fileLeftName = "/file_left";
-    var fileRightName = "/file_right";
-    var fileMergeName = "/file_merged";
-    var javaExtension = ".java";
-    var xmlExtension = ".xml";
+    final var folder = args.length > 0 ? args[0] : "0";
 
     // Source files.
-    var fileBasePath = assetsPath + folder + fileBaseName + javaExtension;
-    var fileLeftPath = assetsPath + folder + fileLeftName + javaExtension;
-    var fileRightPath = assetsPath + folder + fileRightName + javaExtension;
-    var fileMergedPath = assetsPath + folder + fileMergeName + javaExtension;
+    final var fileBasePath = ASSETS_FOLDER_PATH + folder + BASE_FILE_PATH + JAVA_FILE_EXTENSION;
+    final var fileLeftPath = ASSETS_FOLDER_PATH + folder + LEFT_FILE_PATH + JAVA_FILE_EXTENSION;
+    final var fileRightPath = ASSETS_FOLDER_PATH + folder + RIGHT_FILE_PATH + JAVA_FILE_EXTENSION;
+    final var fileMergedPath = ASSETS_FOLDER_PATH + folder + MERGED_FILE_PATH + JAVA_FILE_EXTENSION;
 
     // XML output files.
-    var fileBaseXmlPath = assetsPath + folder + fileBaseName + xmlExtension;
-    var fileLeftXmlPath = assetsPath + folder + fileLeftName + xmlExtension;
-    var fileRightXmlPath = assetsPath + folder + fileRightName + xmlExtension;
-    var fileMergedXmlPath = assetsPath + folder + fileMergeName + xmlExtension;
+    final var fileBaseXmlPath = ASSETS_FOLDER_PATH + folder + BASE_FILE_PATH + XML_FILE_EXTENSION;
+    final var fileLeftXmlPath = ASSETS_FOLDER_PATH + folder + LEFT_FILE_PATH + XML_FILE_EXTENSION;
+    final var fileRightXmlPath = ASSETS_FOLDER_PATH + folder + RIGHT_FILE_PATH + XML_FILE_EXTENSION;
+    final var fileMergedXmlPath = ASSETS_FOLDER_PATH + folder + MERGED_FILE_PATH + XML_FILE_EXTENSION;
     // endregion
 
-    // region Parse source files
-    CompilationUnit fileBaseParsing, fileLeftParsing, fileRightParsing;
+    // region Create matching between branches.
+    final var javaParserGenerator = new JavaParserGenerator();
+
+    // Create parsings.
+    final Tree baseTree, leftTree, rightTree;
     try {
-      fileBaseParsing = StaticJavaParser.parse(Paths.get(fileBasePath));
-      fileLeftParsing = StaticJavaParser.parse(Paths.get(fileLeftPath));
-      fileRightParsing = StaticJavaParser.parse(Paths.get(fileRightPath));
+      baseTree = javaParserGenerator.generateFrom().file(fileBasePath).getRoot();
+      leftTree = javaParserGenerator.generateFrom().file(fileLeftPath).getRoot();
+      rightTree = javaParserGenerator.generateFrom().file(fileRightPath).getRoot();
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
-    // endregion
 
-    // region Serialize parsed files to XML
-    var fileBaseXmlSerializer = new JavaParserToXML(fileBaseParsing);
-    fileBaseXmlSerializer.getXmlDocument().writeToFile(fileBaseXmlPath);
+    // Match the trees.
+    Run.initMatchers();
+    final var matcher = Matchers.getInstance().getMatcher();
+    final var baseLeftMapping = matcher.match(baseTree, leftTree);
+    final var baseRightMapping = matcher.match(baseTree, rightTree);
+    final var leftRightMapping = matcher.match(leftTree, rightTree);
 
-    var fileLeftXmlSerializer = new JavaParserToXML(fileLeftParsing);
-    fileLeftXmlSerializer.getXmlDocument().writeToFile(fileLeftXmlPath);
+    // Print mappings.
+    System.out.println(baseLeftMapping);
 
-    var fileRightXmlSerializer = new JavaParserToXML(fileRightParsing);
-    fileRightXmlSerializer.getXmlDocument().writeToFile(fileRightXmlPath);
-    // endregion
-
-    // region Deserialize XML files to Java
-    if (!Files.exists(Paths.get(fileMergedXmlPath))) {
-      return;
-    }
-    XMLDocument mergedXMLDocument = new XMLDocument(fileMergedXmlPath);
-    var fileMergeXmlDeserializer = new XMLToJavaParser(mergedXMLDocument);
-    var fileMergedNode = fileMergeXmlDeserializer.getAstRoot();
-
-    // Setup LexicalPreservingPrinter
-    LexicalPreservingPrinter.setup(fileMergedNode);
-    
-    // Write the deserialized node to a file.
-    try {
-      Files.write(
-          Paths.get(fileMergedPath), fileMergedNode.toString().getBytes());
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
     // endregion
   }
 }
